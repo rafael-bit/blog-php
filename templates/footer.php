38d02e068f145cf28a8d6100739c92ad05c3da74
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2023-06-29 15:08:22 -0300

    Finish Header and Footer

---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+    <footer>
+        <p><b>1 Blog &copy; 2023</b></p>
+    </footer>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible:wght@400;700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="style/style.css" />
+    <title>1 Blog</title>
+</head>
+<body>
+    <header>
+        <h1><a href="/index.php">1 Blog</a></h1>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Artigos</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
