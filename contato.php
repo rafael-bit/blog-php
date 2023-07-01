bf9560865431869c70fba7438982e897fc79d59e
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2023-07-01 08:33:48 -0300

    update contato

--- contato.php
+++ contato.php
@@ -3,6 +3,14 @@
 ?>
     <?php include_once("templates/header.php");?>
     <main>
-        
+        <div class="contato__container">
+            <div class="contato__content">
+                <h1>Informações de contato</h1>
+                <div class="contato__info">
+                    <span class="material-symbols-outlined">call <p>(11)99999-8888</p></span>
+                    <span class="material-symbols-outlined">mail <p>contato@1blog.com</p></span>
+                </div>
+            </div>
+        </div>
     </main>
     <?php include_once("templates/footer.php");?>
