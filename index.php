5f24d2f2c85484036e901f41cd814345aebad5a4
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2023-07-02 23:01:42 -0300

    init home

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
 ?>
     <?php include_once("templates/header.php");?>
     <main>
-        
+        <h1>1 Blog</h1>
+        <p>O blog sobre as novidades do mundo da tecnologia.</p>
+        <hr />
+        <div class="container">
+            <img src="./img/background.png" alt="foto do código">
+        </div>
+        <article>
+
+        </article>
     </main>
     <?php include_once("templates/footer.php");?>
