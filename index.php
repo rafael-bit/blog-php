20f4a5a7737b784c5923499e05d09ddce5a37335
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2023-07-04 15:39:14 -0300

    updates in code

--- index.php
+++ index.php
@@ -3,22 +3,32 @@
     $ultimoPost = end($posts);
 ?>
     <?php include_once("templates/header.php");?>
-    <main>
-        <div class="container__home">
+    <main class="container">
             <header>
                 <h1>1 Blog</h1>
                 <p>O blog sobre as novidades do mundo da tecnologia.</p>
             </header>
-            <div class="content">
-                <h1>Recente</h1>
-                <a href=""><img src="<?=$ultimoPost['img'] ?>" alt="foto do código"></a>
-                <p class="final">Autor: <?=$ultimoPost['autor']['nome']?></p>
-                <h2><?=$ultimoPost['titulo']?></h2>
-                <p><?= $ultimoPost['conteudo']?></p>
+            <div class="home">
+                <div class="content">
+                    <h1>Recente</h1>
+                    <a href=""><img src="<?=$ultimoPost['img'] ?>" alt="foto do código"></a>
+                    <p class="final">Autor: <?=$ultimoPost['autor']['nome']?></p>
+                    <h2><?=$ultimoPost['titulo']?></h2>
+                    <p><?= $ultimoPost['conteudo']?></p>
+                </div>
+                <article>
+                    <h2>Tag</h2>
+                    <ul>
+                        <?php $catCounts = array_count_values(array_column($posts, 'categoria'));
+                            foreach($posts as $post):
+                                if($catCounts[$post['categoria']] === 1): ?>
+                                    <li><a href="<?= $post['categoria'] ?>.php"><?= $post['categoria'] ?></a></li>
+                                <?php endif;
+                            endforeach;
+                        ?>
+                    </ul>
+                    <h2>Categorias</h2>
+                </article>
             </div>
-        </div>
-        <article>
-
-        </article>
     </main>
     <?php include_once("templates/footer.php");?>
